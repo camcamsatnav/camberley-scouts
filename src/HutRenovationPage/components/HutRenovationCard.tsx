76943da2aed73a332f2bfa074deecb6440e4ba7e
Camden Jones <117047461+camcamsatnav@users.noreply.github.com>
2026-01-12 01:22:08 +0000

    feat: add image gallery to hut renovation page (#9)
    * feat: add image-gallery
    
    * refactor: use Fab component
    
    * test: add image gallery tests
    
    * feat: add keyboard navigation
    
    * style: fix stylelint

--- src/HutRenovationPage/components/HutRenovationCard.tsx
+++ src/HutRenovationPage/components/HutRenovationCard.tsx
@@ -1,30 +1,44 @@
 import Image from '@mui/icons-material/ImageOutlined';
 import { Card, CardContent, CardHeader, CardMedia, IconButton } from '@mui/material';
-import type { ReactElement } from 'react';
+import { type ReactElement, useState } from 'react';
+import type { FileImage } from '../../common/types';
+import { ImageGalleryDialog } from './ImageGalleryDialog';
+
+import '../less/hutRenovationCard.less';
 
 export interface HutRenovationCardProps {
   title: string;
   mainImagePath: string;
   description: string | ReactElement;
-  // TODO: support the additional images -> action button to open a gallery dialog
+  galleryImages: FileImage[];
 }
 
-export const HutRenovationCard = ({ title, mainImagePath, description }: HutRenovationCardProps) => {
+export const HutRenovationCard = ({ title, mainImagePath, description, galleryImages }: HutRenovationCardProps) => {
+
+  const [imageGalleryOpen, setImageGalleryOpen] = useState<boolean>(false);
+
+  const handleOnClick = () => {
+    setImageGalleryOpen(true);
+  };
+
   return (
     <div className='hut-renovation-card' data-testid='hut-renovation-card'>
-      <Card sx={{ width: '100%', display: 'flex', flexDirection: 'column', height: '100%' }}>
-        <CardMedia image={mainImagePath} sx={{ height: 165 }} data-testid='hut-renovation-card-image' />
+      <Card className='hut-renovation-card__card'>
+        <CardMedia
+          className='hut-renovation-card__image' image={mainImagePath} data-testid='hut-renovation-card-image'
+        />
         <CardHeader
           title={title}
           action={
-            <IconButton aria-label={`${title}-image`} data-testid='hut-renovation-card-button'>
+            <IconButton aria-label={`${title}-image`} onClick={handleOnClick} data-testid='hut-renovation-card-button'>
               <Image />
             </IconButton>
           }
         >
         </CardHeader>
-        <CardContent sx={{ flex: '1 1 auto' }}>{description}</CardContent>
+        <CardContent className='hut-renovation-card__content'>{description}</CardContent>
       </Card>
+      <ImageGalleryDialog open={imageGalleryOpen} setOpen={setImageGalleryOpen} images={galleryImages} />
     </div>
   );
 };
---
+++ src/common/types.ts
@@ -0,0 +1,6 @@
+export interface FileImage {
+  /* Absolute path from the public folder e.g /homePage/cubs.png */
+  src: string;
+  /* Alt text for the image */
+  alt: string;
+}
