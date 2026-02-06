cc2b387c5eb2a69b256eab3fd16492baa4167ac7
Camden Jones <117047461+camcamsatnav@users.noreply.github.com>
2026-02-06 18:52:32 +0000

    feat: add location cards to join beavers page (#20)
    * ST-7 - Add location cards to join beavers page
    
    * refactor: copilot comments

---
+++ src/joinBeaversPage/constants.ts
@@ -0,0 +1,17 @@
+import type { LocationCardProps } from './components/LocationCard';
+
+export const BEAVERS_LOCATION_ONE: LocationCardProps = {
+  title: 'join.beavers.location1.title',
+  sessionTimes: 'join.beavers.location1.sessionTimes',
+  addressLines: 'join.beavers.location1.addressLines',
+  googleMapsLink: 'https://maps.app.goo.gl/CFDX869WgfsKcrbs7',
+  spacesAvailable: true,
+};
+
+export const BEAVERS_LOCATION_TWO: LocationCardProps = {
+  title: 'join.beavers.location2.title',
+  sessionTimes: 'join.beavers.location2.sessionTimes',
+  addressLines: 'join.beavers.location2.addressLines',
+  googleMapsLink: 'https://maps.app.goo.gl/Mu9sFMDQs1YHnjRy9',
+  spacesAvailable: false,
+};
