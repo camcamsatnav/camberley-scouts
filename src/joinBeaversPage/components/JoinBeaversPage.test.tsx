bb883e6249a0e6ee8f26cb7b8612565248b7c2b9
Sam Wavish <89749822+swavish164@users.noreply.github.com>
2026-02-20 23:30:14 +0000

    feat: join button on join beavers page (#24)
    * Join Button
    
    * Fix ui
    
    * Fixing comments
    
    * refactor: fix flex
    ST-6
    
    ---------
    
    Co-authored-by: Camden Jones <117047461+camcamsatnav@users.noreply.github.com>

--- src/joinBeaversPage/components/JoinBeaversPage.test.tsx
+++ src/joinBeaversPage/components/JoinBeaversPage.test.tsx
@@ -1,12 +1,21 @@
 import { render, screen } from '@testing-library/react';
 import { expect, it } from 'vitest';
 import { JoinBeaversPage } from './JoinBeaversPage';
+import { BEAVERS_IMAGE_PATH } from '../../HomePage/constants';
 
 it('should render JoinBeaversPage correctly', () => {
   render(<JoinBeaversPage />);
 
   expect(screen.getByTestId('join-beavers-page')).toBeInTheDocument();
   expect(screen.getByTestId('page-heading')).toBeInTheDocument();
+
+  expect(screen.getByText('We have 2 Beaver Colonies')).toBeInTheDocument();
+
+  expect(screen.getByTestId('beavers-logo-image')).toHaveAttribute('src', BEAVERS_IMAGE_PATH);
+
+  expect(screen.getByText('6-8 years')).toBeInTheDocument();
+
+  expect(screen.getByTestId('beavers-join-button')).toBeInTheDocument();
 
   expect(screen.getAllByTestId('location-card')).toHaveLength(2);
 
--- src/joinBeaversPage/components/JoinBeaversPage.tsx
+++ src/joinBeaversPage/components/JoinBeaversPage.tsx
@@ -1,7 +1,8 @@
-import { Card, CardContent, CardHeader } from '@mui/material';
+import { Button, Card, CardContent, CardHeader } from '@mui/material';
 import { useTranslation } from 'react-i18next';
 import { PageHeading } from '../../common/components/PageHeading';
 import { BEAVERS_LOCATION_ONE, BEAVERS_LOCATION_TWO } from '../constants';
+import { BEAVERS_IMAGE_PATH } from '../../HomePage/constants';
 import { LocationCard } from './LocationCard';
 
 import '../less/joinBeaversPage.less';
@@ -16,9 +17,34 @@ export const JoinBeaversPage = () => {
     <div className='join-beavers-page' data-testid='join-beavers-page'>
       <PageHeading title={t('join.beavers.title')} />
       <div className='join-beavers-page__content'>
-        <div className='join-beavers-page__content__cards'>
-          <LocationCard {...BEAVERS_LOCATION_ONE} />
-          <LocationCard {...BEAVERS_LOCATION_TWO} />
+        <div className='join-beavers-page__content__top'>
+          <div className='join-beavers-page__content__top__info'>
+            <p className='join-beavers-page__content__top__info__text'>{t('join.beavers.intro.title')}</p>
+            <div className='join-beavers-page__content__top__info__logo'>
+              <img
+                src={BEAVERS_IMAGE_PATH}
+                alt='Beavers logo and age range'
+                width={200}
+                height={75}
+                data-testid='beavers-logo-image'
+              />
+              <p className='join-beavers-page__content__top__info__logo__age'>{t('join.beavers.intro.age')}</p>
+            </div>
+            <div className='join-beavers-page__content__top__info__button'>
+              <Button
+                color='secondary'
+                variant='contained'
+                size='large'
+                data-testid='beavers-join-button'
+              >
+                {t('home.buttons.join')}
+              </Button>
+            </div>
+          </div>
+          <div className='join-beavers-page__content__top__cards'>
+            <LocationCard {...BEAVERS_LOCATION_ONE} />
+            <LocationCard {...BEAVERS_LOCATION_TWO} />
+          </div>
         </div>
         <div className='join-beavers-page__content__information' data-testid='beavers-info'>
           <ul className='join-beavers-page__content__information__list' data-testid='beavers-info-list'>
